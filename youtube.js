425cd52c3decaa5ed0ca0e95da1a606f857dfafb
Gamepix221 <106008859+Gamepix221@users.noreply.github.com>
2023-01-03 18:46:07 +0100

    Update youtube.js

--- youtube.js
+++ youtube.js
@@ -1,1 +1,12 @@
-
+export default data = [
+    {
+        "titre": "Le chef Otaku",
+        "description": "Attention, sur la chaîne Menu Manga, on a affaire à un sacré passionné, capable de parler pendant 20 minutes d’une série d’une manière très argumentée. Il parle aussi pas mal de séries animées.",
+        "lien": "https://www.youtube.com/embed/hruLH9hjwDc"
+    },
+    {
+        "titre": "Selvarion Senpai",
+        "description": "Romuald est un fou furieux de culture japonaise et donc de manga ! Et en plus, il publie régulièrement des vidéos. Une chaîne très intéressante.",
+        "lien": "https://www.youtube.com/embed/qCablUZUC5A"
+    }
+]
